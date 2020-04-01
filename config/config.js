0ba3e1419c814b412fda6ebb4262d2ea2d3bb316
Helena-Lindahl <helena.l.lindahl@gmail.com>
2020-04-01 09:47:01 +0200

    make it better

--- config/config.js
+++ config/config.js
@@ -1,10 +1,12 @@
+require("dotenv").config()
+
 const config = {
 
     // databaseURL:process.env.DATABASE,
     // mail:process.env.MAIL
 
     databaseURL: "mongodb+srv://Helena:NyttLosen20@cluster0-nlmox.mongodb.net/testmiljo?retryWrites=true&w=majority",
-    //Mail: "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"
+    Mail: "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"
     
 }
 
